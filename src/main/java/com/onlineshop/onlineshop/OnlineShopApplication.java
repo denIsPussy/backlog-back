7ebc881cb00be22313c44778635ceb692e8a3759
Denis <denis.musoev@yandex.ru>
2024-05-29 11:35:21 +0400

    fix

--- src/main/java/com/onlineshop/onlineshop/OnlineShopApplication.java
+++ src/main/java/com/onlineshop/onlineshop/OnlineShopApplication.java
@@ -12,16 +12,4 @@ public class OnlineShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShopApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://backlogshop.ru", "https://backlogshop.ru", "http://185.200.241.178")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*");
-			}
-		};
-	}
 }
--- src/main/java/com/onlineshop/onlineshop/SecurityConfiguration.java
+++ src/main/java/com/onlineshop/onlineshop/SecurityConfiguration.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,9 +27,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfiguration {
     @Autowired
     JwtRequestFilter jwtAuthFilter;
@@ -40,6 +46,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors((cors) -> cors
+                        .configurationSource(corsConfigurationSource())
+                )
                 // Отключаем CSRF защиту, т.к. используем Stateless сессии
                 .csrf(AbstractHttpConfigurer::disable)
                 // Настройка правил авторизации
@@ -54,6 +63,16 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider());
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://backlogshop.ru", "https://backlogshop.ru", "http://185.200.241.178"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
