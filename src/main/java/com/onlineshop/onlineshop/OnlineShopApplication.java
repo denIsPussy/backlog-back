b806604c748a507d2b0d370e6d81d017a41ee2bc
Denis <denis.musoev@yandex.ru>
2024-05-29 11:28:21 +0400

    fix

--- src/main/java/com/onlineshop/onlineshop/OnlineShopApplication.java
+++ src/main/java/com/onlineshop/onlineshop/OnlineShopApplication.java
@@ -2,6 +2,9 @@ package com.onlineshop.onlineshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OnlineShopApplication {
@@ -9,5 +12,16 @@ public class OnlineShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShopApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://backlogshop.ru", "https://backlogshop.ru", "http://185.200.241.178")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
--- src/main/java/com/onlineshop/onlineshop/WebConfig.java
+++ src/main/java/com/onlineshop/onlineshop/WebConfig.java
@@ -5,15 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Разрешаем CORS для всех путей
-                .allowedOrigins("http://backlogshop.ru", "https://backlogshop.ru", "http://185.200.241.178")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // Разрешаем CORS для всех путей
+//                .allowedOrigins("http://backlogshop.ru", "https://backlogshop.ru", "http://185.200.241.178")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*");
+//    }
+//}
