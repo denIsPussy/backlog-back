965a723d9c3e842befdae1c4e3aee2b238c94045
Denis <denis.musoev@yandex.ru>
2024-06-18 19:30:40 +0400

    clean

--- src/main/java/com/onlineshop/onlineshop/Repositories/AttributeRepository.java
+++ src/main/java/com/onlineshop/onlineshop/Repositories/AttributeRepository.java
@@ -3,6 +3,6 @@ package com.onlineshop.onlineshop.Repositories;
 import com.onlineshop.onlineshop.Models.Database.Product.Attribute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttributeRepository extends JpaRepository<Attribute, Integer> {
+    public interface AttributeRepository extends JpaRepository<Attribute, Integer> {
 }
 
--- src/main/java/com/onlineshop/onlineshop/Repositories/CartItemRepository.java
+++ src/main/java/com/onlineshop/onlineshop/Repositories/CartItemRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
-    List<CartItem> findByProductIdAndCartId(int productId, int cartId);
 }
